f123ac62424eb9d21a46e9198f6713e0be965a62
Dan Estrin <danestrin4@gmail.com>
2019-05-24 13:18:24 -0700

    added more headers to roster call, second test on heroku

--- routes/api/teamroster.js
+++ routes/api/teamroster.js
@@ -62,7 +62,12 @@ router.get('/', (req, res) => {
       TeamID: req.parameters.teamId
     },
     headers: {
-      'accept-language':'he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4'
+      host: 'stats.nba.com',
+      "cache-control":"max-age=0",
+      connection: 'keep-alive',
+      "accept-encoding" : "Accepflate, sdch",
+      'accept-language':'he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
     }
   })
     .then((response)=>{
