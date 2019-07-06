df9c09bb6e6e0e86d161ed37570169d4016fefdc
Dan Estrin <danestrin4@gmail.com>
2019-07-05 19:43:48 -0700

    added check to only add regular season score data

--- routes/api/score.js
+++ routes/api/score.js
@@ -13,37 +13,39 @@ router.get('/', (req, res) => {
       var games = [];
 
       for (var i = 0; i < data.games.length; i++) {
-        if (data.games[i].playoffs == null) {
-          var isPlayoffGame = false;
-        } else {
-          var isPlayoffGame = true;
-        }
-
-        var game = {
-          date: req.query.gameDate,
-          gameId: data.games[i].gameId,
-          playoff: isPlayoffGame,
-          round: data.games[i].playoffs == null ? "" : data.games[i].playoffs.roundNum,
-          gameNum: data.games[i].playoffs == null ? "" : data.games[i].playoffs.gameNumInSeries,
-          hTeam: {
-            teamId: data.games[i].hTeam.teamId,
-            triCode: data.games[i].hTeam.triCode,
-            logoSvg: "https://stats.nba.com/media/img/teams/logos/" + data.games[i].hTeam.triCode + "_logo.svg",
-            logoPng: req.protocol + '://' + req.headers.host + '/logos/' + data.games[i].hTeam.triCode + '_logo.png',
-            score: data.games[i].hTeam.score,
-            series: data.games[i].playoffs == null ? "" : data.games[i].playoffs.hTeam.seriesWin
-          },
-          vTeam: {
-            teamId: data.games[i].vTeam.teamId,
-            triCode: data.games[i].vTeam.triCode,
-            logoSvg: "https://stats.nba.com/media/img/teams/logos/" + data.games[i].vTeam.triCode + "_logo.svg",
-            logoPng: req.protocol + '://' + req.headers.host + '/logos/' + data.games[i].vTeam.triCode + '_logo.png',
-            score: data.games[i].vTeam.score,
-            series: data.games[i].playoffs == null ? "" : data.games[i].playoffs.vTeam.seriesWin
+        if (data.games[i].leagueName == "standard") {
+          if (data.games[i].playoffs == null) {
+            var isPlayoffGame = false;
+          } else {
+            var isPlayoffGame = true;
           }
-        }
 
-        games.push(game);
+          var game = {
+            date: req.query.gameDate,
+            gameId: data.games[i].gameId,
+            playoff: isPlayoffGame,
+            round: data.games[i].playoffs == null ? "" : data.games[i].playoffs.roundNum,
+            gameNum: data.games[i].playoffs == null ? "" : data.games[i].playoffs.gameNumInSeries,
+            hTeam: {
+              teamId: data.games[i].hTeam.teamId,
+              triCode: data.games[i].hTeam.triCode,
+              logoSvg: "https://stats.nba.com/media/img/teams/logos/" + data.games[i].hTeam.triCode + "_logo.svg",
+              logoPng: req.protocol + '://' + req.headers.host + '/logos/' + data.games[i].hTeam.triCode + '_logo.png',
+              score: data.games[i].hTeam.score,
+              series: data.games[i].playoffs == null ? "" : data.games[i].playoffs.hTeam.seriesWin
+            },
+            vTeam: {
+              teamId: data.games[i].vTeam.teamId,
+              triCode: data.games[i].vTeam.triCode,
+              logoSvg: "https://stats.nba.com/media/img/teams/logos/" + data.games[i].vTeam.triCode + "_logo.svg",
+              logoPng: req.protocol + '://' + req.headers.host + '/logos/' + data.games[i].vTeam.triCode + '_logo.png',
+              score: data.games[i].vTeam.score,
+              series: data.games[i].playoffs == null ? "" : data.games[i].playoffs.vTeam.seriesWin
+            }
+          }
+
+          games.push(game);
+        }
       }
 
       res.json(games);
